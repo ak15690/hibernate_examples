5c419f13f5c67eb5aa3ad17b7f2c0a8d1258b765
Ayush Kumar <ayushkumar@LAP-C02Z2JAFL40Y.local>
2019-10-18 13:46:21 +0530

    Added examples

---
+++ src/main/java/com/blacbuck/dealbreaker/PersonDTO.java
@@ -0,0 +1,12 @@
+package com.blacbuck.dealbreaker;
+
+import lombok.Data;
+
+@Data
+public class PersonDTO {
+
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+}
